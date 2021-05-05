5839d1ebdd2945e32f818ca64834b09d481fc4c5
ramirobentes <ramirobentes@live.com>
2021-05-05 15:02:12 -0300

    Add files via upload

---
+++ get newpbp.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+library(lubridate)
+library(zoo)
+library(nbastatR)
+library(future)
+
+logs_teams <- game_logs(2021, result_type = "team")
+
+games <- logs_teams %>%
+  select(idGame, slugTeam, slugOpponent, locationGame) %>%
+  mutate(slugTeamHome = ifelse(locationGame == "H", slugTeam, slugOpponent),
+         slugTeamAway = ifelse(locationGame == "A", slugTeam, slugOpponent)) %>%
+  select(-c(slugTeam, slugOpponent, locationGame)) %>%
+  distinct(idGame, .keep_all = TRUE)
+
+plan(multiprocess)
+play_logs_all <- play_by_play_v2(game_ids = unique(games$idGame))
+
+new_pbp <- play_logs_all %>%
+  mutate(numberOriginal = numberEvent) %>%
+  anti_join(correcoes %>%
+              filter(correction == "remove") %>%
+              select(idGame, numberOriginal)) %>% # ver se deixa
+  distinct(idGame, numberEvent, .keep_all = TRUE) %>%   # remove duplicate events
+  mutate(secsLeftQuarter = (minuteRemainingQuarter * 60) + secondsRemainingQuarter) %>%                       
+  mutate(secsStartQuarter = case_when(                                                                        
+    numberPeriod %in% c(1:5) ~ (numberPeriod - 1) * 720,
+    TRUE ~ 2880 + (numberPeriod - 5) * 300
+  )) %>%
+  mutate(secsPassedQuarter = ifelse(numberPeriod %in% c(1:4), 720 - secsLeftQuarter, 300 - secsLeftQuarter),  
+         secsPassedGame = secsPassedQuarter + secsStartQuarter) %>%
+  arrange(idGame, secsPassedGame) %>%
+  filter(numberEventMessageType != 18) %>%     # instant replay
+  group_by(idGame) %>%
+  mutate(numberEvent = row_number()) %>%  # new numberEvent column with events in the right order
+  ungroup() %>%
+  select(idGame, numberOriginal, numberEventMessageType, numberEventActionType, namePlayer1, namePlayer2, namePlayer3,                   
+         slugTeamPlayer1, slugTeamPlayer2,  slugTeamPlayer3, numberPeriod, timeQuarter, secsPassedGame, 
+         descriptionPlayHome, numberEvent, descriptionPlayVisitor, scoreHome, scoreAway,
+         descriptionPlayNeutral) %>%
+  mutate(shotPtsHome = case_when(
+    numberEventMessageType == 3 & !str_detect(descriptionPlayHome, "MISS") ~ 1,                               
+    numberEventMessageType == 1 & str_detect(descriptionPlayHome, "3PT") ~ 3,                                 
+    numberEventMessageType == 1 & !str_detect(descriptionPlayHome, "3PT") ~ 2,
+    TRUE ~ 0
+  )) %>%
+  mutate(shotPtsAway = case_when(
+    numberEventMessageType == 3 & !str_detect(descriptionPlayVisitor, "MISS") ~ 1,
+    numberEventMessageType == 1 & str_detect(descriptionPlayVisitor, "3PT") ~ 3,
+    numberEventMessageType == 1 & !str_detect(descriptionPlayVisitor, "3PT") ~ 2,
+    TRUE ~ 0
+  )) %>%
+  group_by(idGame) %>%
+  mutate(ptsHome = cumsum(shotPtsHome),
+         ptsAway = cumsum(shotPtsAway)) %>%
+  ungroup() %>%
+  left_join(games %>%
+              select(idGame, slugTeamHome, slugTeamAway)) %>%
+  select(idGame, numberOriginal, numberEventMessageType, numberEventActionType, slugTeamHome, slugTeamAway, slugTeamPlayer1,
+         slugTeamPlayer2, slugTeamPlayer3, numberPeriod, timeQuarter, secsPassedGame, numberEvent, namePlayer1, namePlayer2, 
+         namePlayer3, descriptionPlayHome, descriptionPlayVisitor, ptsHome, ptsAway, shotPtsHome, shotPtsAway,
+         descriptionPlayNeutral) %>%
+  mutate(marginBeforeHome = ptsHome - ptsAway - shotPtsHome + shotPtsAway,
+         marginBeforeAway = ptsAway - ptsHome - shotPtsAway + shotPtsHome,
+         timeQuarter = str_pad(timeQuarter, width = 5, pad = 0)) %>%
+  replace_na(list(descriptionPlayHome = "",
+                  descriptionPlayVisitor = "",
+                  descriptionPlayNeutral = ""))
